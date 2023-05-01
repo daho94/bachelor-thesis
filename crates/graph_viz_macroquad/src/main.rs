cf14e62c1d998847482a6e73bb5d6b728d96fd8b
Daniel Holzner <da.holzner@gmail.com>
2023-05-02 00:00:46 +0200

    Dragging works

--- crates/graph_viz_macroquad/src/main.rs
+++ crates/graph_viz_macroquad/src/main.rs
@@ -17,7 +17,7 @@ fn window_conf() -> Conf {
     }
 }
 
-fn draw_graph(lines: &[(Vec2, Vec2)], bbox: &(Vec2, Vec2)) {
+fn draw_graph(lines: &[(Vec2, Vec2)], bbox: &(Vec2, Vec2), zoom: f32, target: Vec2) {
     let screen_size = screen_width().min(screen_height());
     let Vec2 { x: x_min, y: y_min } = bbox.0;
     let Vec2 { x: x_max, y: y_max } = bbox.1;
@@ -25,42 +25,46 @@ fn draw_graph(lines: &[(Vec2, Vec2)], bbox: &(Vec2, Vec2)) {
     let width = x_max - x_min;
     let height = y_max - y_min;
 
-    let scale_x = screen_width() / width;
-    let scale_y = screen_height() / height;
+    // let scale_x = screen_width() / width / zoom;
+    // let scale_y = screen_height() / height / zoom;
+    let scale_x = screen_width() / width / zoom;
+    let scale_y = screen_height() / height / zoom;
 
     let scale = scale_x.min(scale_y);
 
-    let graph_center = vec2((x_max + x_min) / 2.0, (y_max + y_min) / 2.0);
+    // let graph_center = vec2((x_max + x_min) / 2.0, (y_max + y_min) / 2.0);
+    // https://github.com/blitzarx1/egui_graphs/blob/master/src/graph_view.rs#L302
 
-    let offset_x = (screen_width() - width * scale_x) / 2.0;
-    let offset_y = (screen_height() - height * scale_y) / 2.0;
+    let offset_x = target.x + (screen_width() - (width) * scale) / 2.0;
+    let offset_y = target.y + (screen_height() - (height) * scale) / 2.0;
+    // let offset_x = target.x;
+    // let offset_y = target.y;
 
+    let mut rendered_lines = 0;
     // Draw all edges and scale according to screen size
     for (from, to) in lines.iter() {
+        // Move every point by target
         let from = vec2(
-            (from.x - x_min) * scale_x + offset_x,
-            (from.y - y_min) * scale_y + offset_y,
+            (from.x - x_min) * scale + offset_x,
+            (from.y - y_min) * scale + offset_y,
         );
         let to = vec2(
-            (to.x - x_min) * scale_x + offset_x,
-            (to.y - y_min) * scale_y + offset_y,
+            (to.x - x_min) * scale + offset_x,
+            (to.y - y_min) * scale + offset_y,
         );
 
-        // Only render lines where at least one point is visible on screen
-        if from.x < 0.0
-            && from.x > screen_width()
-            && from.y < 0.0
-            && from.y > screen_height()
-            && to.x < 0.0
-            && to.x > screen_width()
-            && to.y < 0.0
-            && to.y > screen_height()
+        // Only render lines where from or to point is inside screen
+        if (from.x < 0.0 || from.x > screen_width() || from.y < 0.0 || from.y > screen_height())
+            && (to.x < 0.0 || to.x > screen_width() || to.y < 0.0 || to.y > screen_height())
         {
             continue;
         }
 
         draw_line(from.x, from.y, to.x, to.y, 1.0, GRAY);
+
+        rendered_lines += 1;
     }
+    draw_circle(screen_width() / 2., screen_height() / 2., 5., RED);
 }
 
 fn spherical_to_cartesian(lat: f32, lon: f32) -> (f32, f32) {
@@ -71,7 +75,7 @@ fn spherical_to_cartesian(lat: f32, lon: f32) -> (f32, f32) {
     (y, x)
 }
 
-fn calc_bbox(graph_bbox: &(Vec2, Vec2), target: (f32, f32), zoom: f32) -> (Vec2, Vec2) {
+fn calc_bbox(graph_bbox: &(Vec2, Vec2), target: Vec2, zoom: f32) -> (Vec2, Vec2) {
     // visible_bbox shrinks when zooming
     let old_width = graph_bbox.1.x - graph_bbox.0.x;
     let old_height = graph_bbox.1.y - graph_bbox.0.y;
@@ -80,12 +84,12 @@ fn calc_bbox(graph_bbox: &(Vec2, Vec2), target: (f32, f32), zoom: f32) -> (Vec2,
     let new_height = old_height * zoom;
     (
         vec2(
-            graph_bbox.0.x + target.0 + (old_width - new_width) / 2.0,
-            graph_bbox.0.y + target.1 + (old_height - new_height) / 2.0,
+            graph_bbox.0.x + target.x + (old_width - new_width) / 2.0,
+            graph_bbox.0.y + target.y + (old_height - new_height) / 2.0,
         ),
         vec2(
-            graph_bbox.1.x + target.0 - (old_width - new_width) / 2.0,
-            graph_bbox.1.y + target.1 - (old_height - new_height) / 2.0,
+            graph_bbox.1.x + target.x - (old_width - new_width) / 2.0,
+            graph_bbox.1.y + target.y - (old_height - new_height) / 2.0,
         ),
     )
 }
@@ -105,7 +109,7 @@ impl Draggable {
         }
     }
 
-    fn update(&mut self, bbox: &(Vec2, Vec2), target: &mut (f32, f32), zoom: f32) {
+    fn update(&mut self, bbox: &(Vec2, Vec2), target: &mut Vec2, zoom: f32) {
         let (x, y) = mouse_position();
         let mouse_position = vec2(x, y);
 
@@ -114,37 +118,10 @@ impl Draggable {
                 self.is_dragging = true;
                 self.last_mouse_position = mouse_position;
             } else {
-                let displacement = (mouse_position - self.last_mouse_position);
-                dbg!(&displacement);
+                let displacement = mouse_position - self.last_mouse_position;
 
-                let w = bbox.1.x - bbox.0.x;
-                let h = bbox.1.y - bbox.0.y;
-                let move_factor = 0.01;
-
-                match displacement {
-                    Vec2 { x: 0.0, y: 0.0 } => {}
-                    Vec2 { x: 0.0, y } => {
-                        target.1 += y.signum() * -move_factor * h;
-                    }
-                    Vec2 { x, y: 0.0 } => {
-                        target.0 += x.signum() * -move_factor * w;
-                    }
-                    Vec2 { x, y } => {
-                        // let angle = y.atan2(x);
-                        // target.0 += x.signum() * -move_factor * w;
-                        // target.1 += angle.tan() * x.signum() * -move_factor * w;
-                        // target.0 += x.signum() * -move_factor * w;
-                        // target.1 += y.signum() * -move_factor * h;
-                        if x.abs() > y.abs() {
-                            target.0 += x.signum() * -move_factor * w;
-                        } else if x.abs() == y.abs() {
-                            target.0 += x.signum() * -move_factor * w;
-                            target.1 += y.signum() * -move_factor * h;
-                        } else {
-                            target.1 += y.signum() * -move_factor * h;
-                        }
-                    }
-                }
+                target.x += displacement.x;
+                target.y += displacement.y;
 
                 self.position += displacement;
                 self.last_mouse_position = mouse_position;
@@ -207,32 +184,33 @@ async fn main() {
 
     // Camera settings
     let mut zoom = 1.0;
-    let mut target = (0., 0.);
+    let mut target = Vec2::default();
     let mut visible_bbox = calc_bbox(&graph_bbox, target, zoom);
 
     let mut draggable = Draggable::new(vec2(0.0, 0.0));
     loop {
+        dbg!(&target);
         clear_background(DARKGRAY);
 
         draggable.update(&visible_bbox, &mut target, zoom);
 
-        let mut move_factor = 0.00001;
+        let mut move_factor = 5.;
         if is_key_down(KeyCode::W) {
-            target.1 -= move_factor;
+            target.y += move_factor;
         }
         if is_key_down(KeyCode::S) {
-            target.1 += move_factor;
+            target.y -= move_factor;
         }
         if is_key_down(KeyCode::A) {
-            target.0 -= move_factor;
+            target.x += move_factor;
         }
         if is_key_down(KeyCode::D) {
-            target.0 += move_factor;
+            target.x -= move_factor;
         }
 
         if is_key_down(KeyCode::R) {
             zoom = 1.0;
-            target = (0.0, 0.0);
+            target = Vec2::default();
         }
 
         // Zoom in and out with mouse wheel
@@ -243,13 +221,23 @@ async fn main() {
                     let zoom_factor = 0.05 * zoom;
                     let new_zoom: f32 = zoom - y.signum() * zoom_factor;
                     zoom = new_zoom.clamp(0.0, 1.0);
+
+                    // let (x, y) = mouse_position();
+                    // let displacement = mouse_position - draggable.last_mouse_position;
+
+                    // target.x = x;
+                    // target.y = y;
+                    // target = Vec2::default();
+
+                    // draggable.position += displacement;
+                    // draggable.last_mouse_position = mouse_position;
                 }
             }
             _ => (),
         }
 
         // Recalculate visible bbox
-        visible_bbox = calc_bbox(&graph_bbox, target, zoom);
+        // visible_bbox = calc_bbox(&graph_bbox, target, zoom);
 
         egui_macroquad::ui(|egui_ctx| {
             egui::Window::new("egui ❤ macroquad").show(egui_ctx, |ui| {
@@ -258,7 +246,7 @@ async fn main() {
         });
 
         // Draw things before egui
-        draw_graph(&lines, &visible_bbox);
+        draw_graph(&lines, &visible_bbox, zoom, target);
 
         egui_macroquad::draw();
 
