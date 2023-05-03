bd75ab668e06bc2f1c53210972035b36dae10df6
Daniel Holzner <da.holzner@gmail.com>
2023-05-03 15:36:35 +0200

    Refactor

--- crates/graph_viz_macroquad/src/main.rs
+++ crates/graph_viz_macroquad/src/main.rs
@@ -1,70 +1,17 @@
-use std::f32::MAX;
+use std::f32::{MAX, MIN};
 
 use ch_core::graph::Graph;
-use egui::gui_zoom::zoom_in;
 use macroquad::prelude::*;
-
-const EARTH_RADIUS: f32 = 6378137.0;
 
 fn window_conf() -> Conf {
     Conf {
         window_title: "Graph View".to_string(),
         fullscreen: false,
         window_resizable: true,
-        window_width: 1280,
-        window_height: 720,
+        window_width: 600,
+        window_height: 520,
         ..Default::default()
     }
-}
-
-fn draw_graph(lines: &[(Vec2, Vec2)], bbox: &(Vec2, Vec2), zoom: f32, target: Vec2) {
-    let screen_size = screen_width().min(screen_height());
-    let Vec2 { x: x_min, y: y_min } = bbox.0;
-    let Vec2 { x: x_max, y: y_max } = bbox.1;
-
-    let width = x_max - x_min;
-    let height = y_max - y_min;
-
-    // let scale_x = screen_width() / width / zoom;
-    // let scale_y = screen_height() / height / zoom;
-    let scale_x = screen_width() / width / zoom;
-    let scale_y = screen_height() / height / zoom;
-
-    let scale = scale_x.min(scale_y);
-
-    // let graph_center = vec2((x_max + x_min) / 2.0, (y_max + y_min) / 2.0);
-    // https://github.com/blitzarx1/egui_graphs/blob/master/src/graph_view.rs#L302
-
-    let offset_x = target.x + (screen_width() - (width) * scale) / 2.0;
-    let offset_y = target.y + (screen_height() - (height) * scale) / 2.0;
-    // let offset_x = target.x;
-    // let offset_y = target.y;
-
-    let mut rendered_lines = 0;
-    // Draw all edges and scale according to screen size
-    for (from, to) in lines.iter() {
-        // Move every point by target
-        let from = vec2(
-            (from.x - x_min) * scale + offset_x,
-            (from.y - y_min) * scale + offset_y,
-        );
-        let to = vec2(
-            (to.x - x_min) * scale + offset_x,
-            (to.y - y_min) * scale + offset_y,
-        );
-
-        // Only render lines where from or to point is inside screen
-        if (from.x < 0.0 || from.x > screen_width() || from.y < 0.0 || from.y > screen_height())
-            && (to.x < 0.0 || to.x > screen_width() || to.y < 0.0 || to.y > screen_height())
-        {
-            continue;
-        }
-
-        draw_line(from.x, from.y, to.x, to.y, 1.0, GRAY);
-
-        rendered_lines += 1;
-    }
-    draw_circle(screen_width() / 2., screen_height() / 2., 5., RED);
 }
 
 fn spherical_to_cartesian(lat: f32, lon: f32) -> (f32, f32) {
@@ -75,24 +22,6 @@ fn spherical_to_cartesian(lat: f32, lon: f32) -> (f32, f32) {
     (y, x)
 }
 
-fn calc_bbox(graph_bbox: &(Vec2, Vec2), target: Vec2, zoom: f32) -> (Vec2, Vec2) {
-    // visible_bbox shrinks when zooming
-    let old_width = graph_bbox.1.x - graph_bbox.0.x;
-    let old_height = graph_bbox.1.y - graph_bbox.0.y;
-
-    let new_width = old_width * zoom;
-    let new_height = old_height * zoom;
-    (
-        vec2(
-            graph_bbox.0.x + target.x + (old_width - new_width) / 2.0,
-            graph_bbox.0.y + target.y + (old_height - new_height) / 2.0,
-        ),
-        vec2(
-            graph_bbox.1.x + target.x - (old_width - new_width) / 2.0,
-            graph_bbox.1.y + target.y - (old_height - new_height) / 2.0,
-        ),
-    )
-}
 #[derive(Debug)]
 struct Draggable {
     position: Vec2,
@@ -109,7 +38,7 @@ impl Draggable {
         }
     }
 
-    fn update(&mut self, bbox: &(Vec2, Vec2), target: &mut Vec2, zoom: f32) {
+    fn update(&mut self, offset: &mut Vec2, _zoom: f32) {
         let (x, y) = mouse_position();
         let mouse_position = vec2(x, y);
 
@@ -120,8 +49,8 @@ impl Draggable {
             } else {
                 let displacement = mouse_position - self.last_mouse_position;
 
-                target.x += displacement.x;
-                target.y += displacement.y;
+                offset.x += displacement.x;
+                offset.y += displacement.y;
 
                 self.position += displacement;
                 self.last_mouse_position = mouse_position;
@@ -142,6 +71,7 @@ async fn main() {
 
     let mut lines = Vec::with_capacity(g.edges.len());
 
+    let (mut x_min, mut y_min, mut x_max, mut y_max) = (MAX, MAX, MIN, MIN);
     // Transform all coordinates from spherical to cartesian
     // Also find the bounding box of the graph
     for edge in &g.edges {
@@ -156,45 +86,28 @@ async fn main() {
             g.node(to).unwrap().lon as f32,
         );
 
+        x_min = x_min.min(x_from).min(x_to);
+        y_min = y_min.min(y_from).min(y_to);
+        x_max = x_max.max(x_from).max(x_to);
+        y_max = y_max.max(y_from).max(y_to);
+
         lines.push((vec2(x_from, y_from), vec2(x_to, y_to)));
     }
 
-    let graph_bbox = (
-        vec2(
-            lines
-                .iter()
-                .map(|(from, to)| from.x.min(to.x))
-                .fold(f32::INFINITY, f32::min),
-            lines
-                .iter()
-                .map(|(from, to)| from.y.min(to.y))
-                .fold(f32::INFINITY, f32::min),
-        ),
-        vec2(
-            lines
-                .iter()
-                .map(|(from, to)| from.x.max(to.x))
-                .fold(f32::NEG_INFINITY, f32::max),
-            lines
-                .iter()
-                .map(|(from, to)| from.y.max(to.y))
-                .fold(f32::NEG_INFINITY, f32::max),
-        ),
-    );
+    let graph_rect = dbg!(Rect::new(x_min, y_min, x_max - x_min, y_max - y_min));
 
-    // Camera settings
     let mut zoom = 1.0;
     let mut target = Vec2::default();
-    let mut visible_bbox = calc_bbox(&graph_bbox, target, zoom);
+    let mut current_rect = graph_rect;
 
     let mut draggable = Draggable::new(vec2(0.0, 0.0));
     loop {
-        dbg!(&target);
-        clear_background(DARKGRAY);
+        // 1b1b1b
+        clear_background(Color::from_rgba(27, 27, 27, 255));
 
-        draggable.update(&visible_bbox, &mut target, zoom);
+        draggable.update(&mut target, zoom);
 
-        let mut move_factor = 5.;
+        let move_factor = 5.;
         if is_key_down(KeyCode::W) {
             target.y += move_factor;
         }
@@ -211,27 +124,18 @@ async fn main() {
         if is_key_down(KeyCode::R) {
             zoom = 1.0;
             target = Vec2::default();
+            current_rect = graph_rect;
         }
 
         // Zoom in and out with mouse wheel
         match mouse_wheel() {
             (_x, y) if y != 0.0 => {
-                if is_key_down(KeyCode::LeftControl) {
-                    // Increase zoom speed linearly
-                    let zoom_factor = 0.05 * zoom;
-                    let new_zoom: f32 = zoom - y.signum() * zoom_factor;
-                    zoom = new_zoom.clamp(0.0, 1.0);
-
-                    // let (x, y) = mouse_position();
-                    // let displacement = mouse_position - draggable.last_mouse_position;
-
-                    // target.x = x;
-                    // target.y = y;
-                    // target = Vec2::default();
-
-                    // draggable.position += displacement;
-                    // draggable.last_mouse_position = mouse_position;
-                }
+                // Increase zoom speed linearly
+                // let zoom_factor = 0.05 * zoom;
+                // let new_zoom: f32 = zoom - y.signum() * zoom_factor;
+                // zoom = new_zoom.clamp(0.0, 1.0);
+                let zoom = 1. + y.signum() * 0.05;
+                handle_zoom(&mut current_rect, zoom);
             }
             _ => (),
         }
@@ -246,7 +150,12 @@ async fn main() {
         });
 
         // Draw things before egui
-        draw_graph(&lines, &visible_bbox, zoom, target);
+
+        // Handle pan
+        handle_pan(&mut current_rect, &mut target);
+
+        // Handle zoom
+        draw_graph(&lines, &current_rect);
 
         egui_macroquad::draw();
 
@@ -254,4 +163,59 @@ async fn main() {
 
         next_frame().await;
     }
+}
+
+fn draw_graph(lines: &[(Vec2, Vec2)], rect: &Rect) {
+    let scale_x = screen_width() / rect.w;
+    let scale_y = screen_height() / rect.h;
+
+    // Use smallest scale to avoid distortion
+    let scale = scale_x.min(scale_y);
+
+    let mut rendered_lines = 0;
+    // Draw all edges and scale according to screen size
+    for (from, to) in lines.iter() {
+        // Move every point by target
+        let from = vec2((from.x - rect.x) * scale, (from.y - rect.y) * scale);
+        let to = vec2((to.x - rect.x) * scale, (to.y - rect.y) * scale);
+
+        // Only render lines where from or to point is inside screen
+        if (from.x < 0.0 || from.x > screen_width() || from.y < 0.0 || from.y > screen_height())
+            && (to.x < 0.0 || to.x > screen_width() || to.y < 0.0 || to.y > screen_height())
+        {
+            continue;
+        }
+
+        draw_line(
+            from.x,
+            from.y,
+            to.x,
+            to.y,
+            1.0,
+            Color::from_rgba(128, 128, 128, 255),
+        );
+
+        rendered_lines += 1;
+    }
+    draw_circle(screen_width() / 2., screen_height() / 2., 5., RED);
+}
+
+fn handle_zoom(rect: &mut Rect, zoom: f32) {
+    let center = rect.center();
+
+    rect.scale(zoom, zoom);
+    let new_center = rect.center();
+    let diff = center - new_center;
+    rect.move_to(diff + vec2(rect.x, rect.y));
+}
+
+fn handle_pan(rect: &mut Rect, offset: &mut Vec2) {
+    let scale_x = screen_width() / rect.w;
+    let scale_y = screen_height() / rect.h;
+
+    // Use smallest scale to avoid distortion
+    let scale = scale_x.min(scale_y);
+    rect.move_to(-*offset / scale + vec2(rect.x, rect.y));
+    offset.x = 0.;
+    offset.y = 0.;
 }
