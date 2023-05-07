7ebbd6f98212bf1e9e681be778afec004198f850
Daniel Holzner <da.holzner@gmail.com>
2023-05-07 18:53:49 +0200

    Refactor

--- crates/graph_viz_macroquad/src/main.rs
+++ crates/graph_viz_macroquad/src/main.rs
@@ -1,8 +1,7 @@
-use std::f32::{MAX, MIN};
-
 use ch_core::graph::Graph;
 use macroquad::prelude::*;
 
+mod graph_view;
 mod widgets;
 
 fn window_conf() -> Conf {
@@ -14,14 +13,6 @@ fn window_conf() -> Conf {
         window_height: 720,
         ..Default::default()
     }
-}
-
-fn spherical_to_cartesian(lat: f32, lon: f32) -> (f32, f32) {
-    let lat = lat.to_radians();
-    let lon = lon.to_radians();
-    let x = lat.cos() * lon.cos();
-    let y = lat.cos() * lon.sin();
-    (y, x)
 }
 
 #[derive(Debug)]
@@ -69,85 +60,48 @@ async fn main() {
         r"F:\Dev\uni\BA\bachelor_thesis\crates\osm_reader\data\vaterstetten_pp.osm.pbf".into(),
     );
 
-    let g = Graph::<u32>::from_pbf(std::path::Path::new(&pbf_path)).unwrap();
-
-    let mut lines = Vec::with_capacity(g.edges_out.len());
-
-    let (mut x_min, mut y_min, mut x_max, mut y_max) = (MAX, MAX, MIN, MIN);
-    // Transform all coordinates from spherical to cartesian
-    // Also find the bounding box of the graph
-    for edge in g.edges() {
-        let from = edge.source;
-        let to = edge.target;
-        let (x_from, y_from) = spherical_to_cartesian(
-            g.node(from).unwrap().lat as f32,
-            g.node(from).unwrap().lon as f32,
-        );
-        let (x_to, y_to) = spherical_to_cartesian(
-            g.node(to).unwrap().lat as f32,
-            g.node(to).unwrap().lon as f32,
-        );
-
-        x_min = x_min.min(x_from).min(x_to);
-        y_min = y_min.min(y_from).min(y_to);
-        x_max = x_max.max(x_from).max(x_to);
-        y_max = y_max.max(y_from).max(y_to);
-
-        lines.push((vec2(x_from, y_from), vec2(x_to, y_to)));
-    }
-
-    let graph_rect = dbg!(Rect::new(x_min, y_min, x_max - x_min, y_max - y_min));
-
     let mut zoom = 1.0;
-    let mut target = Vec2::default();
-    let mut current_rect = graph_rect;
-
+    let mut pan = Vec2::default();
     let mut draggable = Draggable::new(vec2(0.0, 0.0));
+
+    let g = Graph::<u32>::from_pbf(std::path::Path::new(&pbf_path)).unwrap();
+    let mut graph_view = graph_view::GraphView::new(&g);
 
     // Init egui widgets
     let mut debug_widget = widgets::debug::DebugWidget::new();
 
     loop {
-        // 1b1b1b
         clear_background(Color::from_rgba(27, 27, 27, 255));
 
-        draggable.update(&mut target, zoom);
+        draggable.update(&mut pan, zoom);
 
         let move_factor = 5.;
         if is_key_down(KeyCode::W) {
-            target.y += move_factor;
+            pan.y += move_factor;
         }
         if is_key_down(KeyCode::S) {
-            target.y -= move_factor;
+            pan.y -= move_factor;
         }
         if is_key_down(KeyCode::A) {
-            target.x += move_factor;
+            pan.x += move_factor;
         }
         if is_key_down(KeyCode::D) {
-            target.x -= move_factor;
+            pan.x -= move_factor;
         }
 
         if is_key_down(KeyCode::R) {
             zoom = 1.0;
-            target = Vec2::default();
-            current_rect = graph_rect;
+            pan = Vec2::default();
+            graph_view.reset();
         }
 
         // Zoom in and out with mouse wheel
         match mouse_wheel() {
             (_x, y) if y != 0.0 => {
-                // Increase zoom speed linearly
-                // let zoom_factor = 0.05 * zoom;
-                // let new_zoom: f32 = zoom - y.signum() * zoom_factor;
-                // zoom = new_zoom.clamp(0.0, 1.0);
-                let zoom = 1. + y.signum() * 0.05;
-                handle_zoom(&mut current_rect, zoom);
+                zoom = 1. + y.signum() * 0.05;
             }
             _ => (),
         }
-
-        // Recalculate visible bbox
-        // visible_bbox = calc_bbox(&graph_bbox, target, zoom);
 
         egui_macroquad::ui(|egui_ctx| {
             debug_widget.update(egui_ctx);
@@ -155,11 +109,10 @@ async fn main() {
 
         // Draw things before egui
 
-        // Handle pan
-        handle_pan(&mut current_rect, &mut target);
+        graph_view.update(zoom, pan);
 
-        // Handle zoom
-        draw_graph(&lines, &current_rect);
+        zoom = 1.0;
+        pan = Vec2::default();
 
         egui_macroquad::draw();
 
@@ -167,58 +120,4 @@ async fn main() {
 
         next_frame().await;
     }
-}
-
-fn draw_graph(lines: &[(Vec2, Vec2)], rect: &Rect) {
-    let scale_x = screen_width() / rect.w;
-    let scale_y = screen_height() / rect.h;
-
-    // Use smallest scale to avoid distortion
-    let scale = scale_x.min(scale_y);
-
-    let mut rendered_lines = 0;
-    // Draw all edges and scale according to screen size
-    for (from, to) in lines.iter() {
-        // Move every point by target
-        let from = vec2((from.x - rect.x) * scale, (from.y - rect.y) * scale);
-        let to = vec2((to.x - rect.x) * scale, (to.y - rect.y) * scale);
-
-        // Only render lines where from or to point is inside screen
-        if (from.x < 0.0 || from.x > screen_width() || from.y < 0.0 || from.y > screen_height())
-            && (to.x < 0.0 || to.x > screen_width() || to.y < 0.0 || to.y > screen_height())
-        {
-            continue;
-        }
-
-        draw_line(
-            from.x,
-            from.y,
-            to.x,
-            to.y,
-            1.0,
-            Color::from_rgba(128, 128, 128, 255),
-        );
-
-        rendered_lines += 1;
-    }
-}
-
-fn handle_zoom(rect: &mut Rect, zoom: f32) {
-    let center = rect.center();
-
-    rect.scale(zoom, zoom);
-    let new_center = rect.center();
-    let diff = center - new_center;
-    rect.move_to(diff + vec2(rect.x, rect.y));
-}
-
-fn handle_pan(rect: &mut Rect, offset: &mut Vec2) {
-    let scale_x = screen_width() / rect.w;
-    let scale_y = screen_height() / rect.h;
-
-    // Use smallest scale to avoid distortion
-    let scale = scale_x.min(scale_y);
-    rect.move_to(-*offset / scale + vec2(rect.x, rect.y));
-    offset.x = 0.;
-    offset.y = 0.;
 }
