7fa2b6cf1b59bc776717afc8d0f44599677cb53c
Daniel Holzner <da.holzner@gmail.com>
2023-05-20 16:26:49 +0200

    Implement node contraction with lazyupdate on neighbor nodes

--- crates/ch_core/src/node_contraction.rs
+++ crates/ch_core/src/node_contraction.rs
@@ -1,12 +1,13 @@
-use std::{cmp::Reverse, collections::BinaryHeap};
+use std::{cmp::Reverse, collections::BinaryHeap, time::Instant};
 
-use log::info;
+use log::{debug, info};
 use priority_queue::PriorityQueue;
 use rustc_hash::FxHashSet;
 
 use crate::{
     constants::Weight,
     graph::{node_index, Edge, EdgeIndex, Graph, NodeIndex},
+    search_graph::SearchGraph,
     witness_search::WitnessSearch,
 };
 
@@ -81,17 +82,50 @@ fn calc_edge_difference(v: NodeIndex, g: &Graph) -> i32 {
 /// TODO: Find the best node order
 /// 1. Calculate edge difference for each node and put them in a priority queue. This is the initial node order.
 ///     - Edge difference: Removed edges - shortcut edges
-pub fn contract_nodes(g: &mut Graph) {
+pub fn contract_nodes(g: &mut Graph) -> SearchGraph {
+    let mut search_graph = SearchGraph::with_capacity(g.nodes.len(), g.edges.len());
+
     let mut queue = calc_initial_node_order(g);
 
     while !queue.is_empty() {
         let node = queue.pop().unwrap().0;
+        debug!("=> Contracting node: {}", node.index());
 
         // Contracte node
-        // contract_node(&mut g, node);
+        contract_node(g, node);
 
-        // Update neighbors of node
+        let mut neighbors = FxHashSet::default();
+
+        for (in_idx, in_edge) in g.neighbors_incoming(node) {
+            neighbors.insert(in_edge.source);
+            search_graph.edges_bwd[node.index()].push(in_idx);
+        }
+
+        for (out_idx, out_edge) in g.neighbors_outgoing(node) {
+            neighbors.insert(out_edge.target);
+            search_graph.edges_fwd[node.index()].push(out_idx);
+        }
+
+        // Update priority of neighbors
+        for neighbor in neighbors {
+            let edge_difference = calc_edge_difference(neighbor, g);
+            if let Some(Reverse(old_value)) =
+                queue.change_priority(&neighbor, Reverse(edge_difference))
+            {
+                if edge_difference != old_value {
+                    debug!(
+                        "[Update] Changed priority of node {} from {} to {}",
+                        neighbor.index(),
+                        old_value,
+                        edge_difference
+                    );
+                }
+            }
+        }
     }
+
+    search_graph.edges = g.edges.clone();
+    search_graph
 }
 
 /// Contract nodes in the graph by a given order.
@@ -101,58 +135,76 @@ pub fn contract_nodes(g: &mut Graph) {
 /// u1-->v-->w2
 ///    /    \    
 ///  u2      w3
-pub fn contract_nodes_with_order(g: &mut Graph, node_order: &[NodeIndex]) {
+pub fn contract_nodes_with_order(g: &mut Graph, node_order: &[NodeIndex]) -> SearchGraph {
+    let mut search_graph = SearchGraph::new(g.nodes.len());
+    let now = Instant::now();
     info!("Contracting nodes");
-    for v in node_order {
-        let v = *v;
-        let edges_in: Vec<(EdgeIndex, Edge)> = g
-            .neighbors_incoming(v)
-            .map(|(i, e)| (i, e.clone()))
-            .collect();
+    for node in node_order {
+        let node = *node;
 
-        let edges_out: Vec<(EdgeIndex, Edge)> = g
-            .neighbors_outgoing(v)
-            .map(|(i, e)| (i, e.clone()))
-            .collect();
+        contract_node(g, node);
 
-        for (uv_idx, uv) in edges_in.iter() {
-            let mut max_weight = 0.0;
-            let mut target_nodes = Vec::new();
-            // Calculate max_weight <u,v,w>
-            for (_, vw) in edges_out.iter() {
-                if uv.source == vw.target {
-                    continue;
-                }
-
-                let weight = uv.weight + vw.weight;
-                if weight > max_weight {
-                    max_weight = weight;
-                }
-                target_nodes.push(vw.target);
-            }
-
-            // Start seach from u
-            let ws = WitnessSearch::new(g);
-            let res = ws.search(uv.source, &target_nodes, v, max_weight);
-
-            // Add shortcut if no better path <u,...,w> was found
-            for (vw_idx, vw) in edges_out.iter() {
-                if uv.source == vw.target {
-                    continue;
-                }
-
-                let weight = uv.weight + vw.weight;
-                if weight < *res.get(&vw.target).unwrap_or(&std::f64::INFINITY) {
-                    let shortcut =
-                        Edge::new_shortcut(uv.source, vw.target, weight, [*uv_idx, *vw_idx]);
-
-                    g.add_edge(shortcut);
-                }
-            }
+        for (in_idx, in_edge) in g.neighbors_incoming(node) {
+            search_graph.edges_bwd[node.index()].push(in_idx);
         }
 
-        g.disconnect_node(v);
+        for (out_idx, out_edge) in g.neighbors_outgoing(node) {
+            search_graph.edges_fwd[node.index()].push(out_idx);
+        }
     }
+    info!("Contracting nodes took {:?}", now.elapsed());
+
+    search_graph.edges = g.edges.clone();
+    search_graph
+}
+
+fn contract_node(g: &mut Graph, v: NodeIndex) {
+    let edges_in: Vec<(EdgeIndex, Edge)> = g
+        .neighbors_incoming(v)
+        .map(|(i, e)| (i, e.clone()))
+        .collect();
+
+    let edges_out: Vec<(EdgeIndex, Edge)> = g
+        .neighbors_outgoing(v)
+        .map(|(i, e)| (i, e.clone()))
+        .collect();
+
+    for (uv_idx, uv) in edges_in.iter() {
+        let mut max_weight = 0.0;
+        let mut target_nodes = Vec::new();
+        // Calculate max_weight <u,v,w>
+        for (_, vw) in edges_out.iter() {
+            if uv.source == vw.target {
+                continue;
+            }
+
+            let weight = uv.weight + vw.weight;
+            if weight > max_weight {
+                max_weight = weight;
+            }
+            target_nodes.push(vw.target);
+        }
+
+        // Start seach from u
+        let ws = WitnessSearch::new(g);
+        let res = ws.search(uv.source, &target_nodes, v, max_weight);
+
+        // Add shortcut if no better path <u,...,w> was found
+        for (vw_idx, vw) in edges_out.iter() {
+            if uv.source == vw.target {
+                continue;
+            }
+
+            let weight = uv.weight + vw.weight;
+            if weight < *res.get(&vw.target).unwrap_or(&std::f64::INFINITY) {
+                let shortcut = Edge::new_shortcut(uv.source, vw.target, weight, [*uv_idx, *vw_idx]);
+
+                g.add_edge(shortcut);
+            }
+        }
+    }
+
+    g.disconnect_node(v);
 }
 
 #[cfg(test)]
@@ -160,41 +212,38 @@ mod tests {
     use crate::{
         edge,
         graph::{DefaultIdx, Node},
+        util::test_graphs::{generate_complex_graph, generate_simple_graph},
     };
 
     use super::*;
 
+    fn init() {
+        let _ = env_logger::builder().is_test(true).try_init();
+    }
+
     #[test]
-    fn contract_simple_graph() {
-        //      7 -> 8 -> 9
-        //      |         |
-        // 0 -> 5 -> 6 -  |
-        // |         |  \ |
-        // 1 -> 2 -> 3 -> 4
-        let mut g = Graph::<DefaultIdx>::new();
+    fn contract_simple_graph_with_order() {
+        //           B
+        //           |
+        // E -> A -> C
+        //      |  /
+        //      D
+        init();
+        let mut g = generate_simple_graph();
 
-        for i in 0..10 {
-            g.add_node(Node::new(i, 0.0, 0.0));
-        }
+        // A,E,D,C,B
+        let node_order = vec![
+            node_index(0),
+            node_index(4),
+            node_index(3),
+            node_index(2),
+            node_index(1),
+        ];
 
-        g.add_edge(Edge::new(node_index(0), node_index(1), 1.0));
-        g.add_edge(Edge::new(node_index(1), node_index(2), 1.0));
-        g.add_edge(Edge::new(node_index(2), node_index(3), 1.0));
-        g.add_edge(Edge::new(node_index(3), node_index(4), 20.0));
-        g.add_edge(Edge::new(node_index(0), node_index(5), 5.0));
-        g.add_edge(Edge::new(node_index(5), node_index(6), 1.0));
-        g.add_edge(Edge::new(node_index(6), node_index(4), 20.0));
-        g.add_edge(Edge::new(node_index(6), node_index(3), 20.0));
-        g.add_edge(Edge::new(node_index(5), node_index(7), 5.0));
-        g.add_edge(Edge::new(node_index(7), node_index(8), 1.0));
-        g.add_edge(Edge::new(node_index(8), node_index(9), 1.0));
-        g.add_edge(Edge::new(node_index(9), node_index(4), 1.0));
-
-        let node_order = (0..10).map(node_index).collect::<Vec<_>>();
         contract_nodes_with_order(&mut g, &node_order);
 
         let shortcuts = g.edges().filter(|e| e.is_shortcut()).count();
-        assert_eq!(0, shortcuts)
+        assert_eq!(2, shortcuts)
     }
 
     #[test]
@@ -219,52 +268,8 @@ mod tests {
 
     #[test]
     // https://jlazarsfeld.github.io/ch.150.project/sections/8-contraction/
-    fn contract_complex_graph() {
-        let mut g = Graph::new();
-
-        // 'A'..='K'
-        for i in 0..11 {
-            g.add_node(Node::new(i, 0.0, 0.0));
-        }
-
-        g.add_edges(edge!(0, 1, 3.0)); // A <=> B
-        g.add_edges(edge!(0, 2, 5.0)); // A <=> C
-        g.add_edges(edge!(0, 10, 3.0)); // A <=> K
-
-        g.add_edges(edge!(1, 3, 5.0)); // B <=> D
-        g.add_edges(edge!(1, 2, 3.0)); // B <=> C
-
-        g.add_edges(edge!(2, 3, 2.0)); // C <=> D
-        g.add_edges(edge!(2, 9, 2.0)); // C <=> J
-
-        g.add_edges(edge!(3, 9, 4.0)); // D <=> J
-        g.add_edges(edge!(3, 4, 7.0)); // D <=> E
-
-        g.add_edges(edge!(4, 9, 3.0)); // E <=> J
-        g.add_edges(edge!(4, 5, 6.0)); // E <=> F
-
-        g.add_edges(edge!(5, 7, 2.0)); // F <=> H
-        g.add_edges(edge!(5, 6, 4.0)); // F <=> G
-
-        g.add_edges(edge!(6, 7, 3.0)); // G <=> H
-        g.add_edges(edge!(6, 8, 5.0)); // G <=> I
-
-        g.add_edges(edge!(7, 8, 3.0)); // H <=> I
-        g.add_edges(edge!(7, 9, 2.0)); // H <=> J
-
-        g.add_edges(edge!(8, 9, 4.0)); // I <=> J
-        g.add_edges(edge!(8, 10, 6.0)); // I <=> K
-
-        g.add_edges(edge!(9, 10, 3.0)); // J <=> K
-
-        let mut order = calc_initial_node_order(&g);
-
-        let mut node_order2 = Vec::new();
-        for _ in 0..11 {
-            let p = order.pop().unwrap();
-            node_order2.push(p.0);
-        }
-        dbg!(&node_order2);
+    fn contract_complex_graph_with_order() {
+        let mut g = generate_complex_graph();
 
         // [B, E, I, K, D, G, C, J, H, F, A]
         let node_order = vec![
@@ -288,6 +293,18 @@ mod tests {
         assert_eq!(3 * 2, shortcuts);
     }
 
+    #[test]
+    fn contract_complex_graph() {
+        init();
+        let mut g = generate_complex_graph();
+
+        contract_nodes(&mut g);
+
+        // Display number of shortcuts
+        let shortcuts = g.edges().filter(|e| e.is_shortcut()).count();
+        assert_eq!(2, shortcuts);
+    }
+
     #[ignore = "Takes too long"]
     #[test]
     fn vaterstetten_works() {
@@ -308,7 +325,8 @@ mod tests {
             node_order.push(p.0);
         }
 
-        contract_nodes_with_order(&mut g, &node_order);
+        // contract_nodes_with_order(&mut g, &node_order);
+        contract_nodes(&mut g);
 
         // 46198 - Node order 0,1,2...
         // 11771 - Calculated node order
