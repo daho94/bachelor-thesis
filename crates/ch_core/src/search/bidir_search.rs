d5f1d43efba223f4ca56f1080fa0a0ae6efcc977
Daniel Holzner <da.holzner@gmail.com>
2023-08-05 21:52:10 +0200

    Add parallel implementation of search

--- crates/ch_core/src/search/bidir_search.rs
+++ crates/ch_core/src/search/bidir_search.rs
@@ -57,49 +57,171 @@ impl<'a> BiDirSearch<'a> {
             source, target
         );
 
-        let mut queue_fwd = BinaryHeap::new();
-        let mut queue_bwd = BinaryHeap::new();
-
-        self.data_fwd.insert(source, (0.0, None));
-        self.data_bwd.insert(target, (0.0, None));
-
-        let mut intersect_node = None;
-
-        queue_fwd.push(Candidate::new(source, 0.0));
-        queue_bwd.push(Candidate::new(target, 0.0));
-
         // Do a full dijkstra on upward graph
-        'outer: while !queue_fwd.is_empty() {
-            if let Some(cand) = queue_fwd.pop() {
-                // Stall on demand optimization
-                for (_, edge) in self.g.edges_bwd(cand.node_idx) {
-                    if let Some((dist, _)) = self.data_fwd.get(&edge.source) {
-                        if *dist + edge.weight < cand.weight {
-                            continue 'outer;
-                        }
-                    }
-                }
+        self.fwd_search(source);
 
-                for (edge_idx, edge) in self.g.edges_fwd(cand.node_idx) {
-                    let new_distance = cand.weight + edge.weight;
-                    if new_distance
-                        < self
-                            .data_fwd
-                            .get(&edge.target)
-                            .unwrap_or(&(std::f64::INFINITY, None))
-                            .0
-                    {
-                        self.data_fwd
-                            .insert(edge.target, (new_distance, Some(edge_idx)));
-                        queue_fwd.push(Candidate::new(edge.target, new_distance));
-                    }
-                }
-                self.stats.nodes_settled += 1;
-                self.settled_fwd.insert(cand.node_idx);
+        // Do a full dijkstra on downward graph
+        self.bwd_search(target);
+
+        // Find the set `I` of nodes settled in both dijkstras
+        let intersect = self.settled_fwd.intersection(&self.settled_bwd);
+        let mut intersect_node = None;
+        // Find
+        // dist(s,t) = min { dist(s,v) + dist(v,t) | v in I}
+        // and remember intersect node `v`
+        let mut min_dist = std::f64::INFINITY;
+        for node in intersect {
+            let dist_fwd = self.data_fwd.get(node).unwrap().0;
+            let dist_bwd = self.data_bwd.get(node).unwrap().0;
+
+            let dist = dist_fwd + dist_bwd;
+            if dist < min_dist {
+                min_dist = dist;
+                intersect_node = Some(*node);
             }
         }
 
-        // Do a full dijkstra on downward graph
+        debug!("Intersection node: {:?}", intersect_node);
+        debug!("min {{ dist(s,v) + dist(t,v) | v in I }} = {}", min_dist);
+
+        self.stats.finish();
+
+        self.reconstruct_shortest_path(intersect_node, source)
+    }
+
+    /// Performs a bidirectional search on the graph. Forward and backward search are run in parallel.
+    pub fn search_par(&mut self, source: NodeIndex, target: NodeIndex) -> Option<ShortestPath> {
+        self.init();
+        info!(
+            "BEGIN BIDIRECTIONAL SEARCH from {:?} to {:?}",
+            source, target
+        );
+
+        std::thread::scope(|s| {
+            let handle_fwd = s.spawn(|| {
+                info!("Start forward search");
+                let mut nodes_settled = 0;
+                let mut queue_fwd = BinaryHeap::new();
+                queue_fwd.push(Candidate::new(source, 0.0));
+
+                let mut data_fwd = FxHashMap::default();
+                let mut settled_fwd = FxHashSet::default();
+
+                data_fwd.insert(source, (0.0, None));
+
+                'outer: while !queue_fwd.is_empty() {
+                    if let Some(cand) = queue_fwd.pop() {
+                        // Stall on demand optimization
+                        for (_, edge) in self.g.edges_bwd(cand.node_idx) {
+                            if let Some((dist, _)) = data_fwd.get(&edge.source) {
+                                if *dist + edge.weight < cand.weight {
+                                    continue 'outer;
+                                }
+                            }
+                        }
+
+                        for (edge_idx, edge) in self.g.edges_fwd(cand.node_idx) {
+                            let new_distance = cand.weight + edge.weight;
+                            if new_distance
+                                < data_fwd
+                                    .get(&edge.target)
+                                    .unwrap_or(&(std::f64::INFINITY, None))
+                                    .0
+                            {
+                                data_fwd.insert(edge.target, (new_distance, Some(edge_idx)));
+                                queue_fwd.push(Candidate::new(edge.target, new_distance));
+                            }
+                        }
+                        nodes_settled += 1;
+                        settled_fwd.insert(cand.node_idx);
+                    }
+                }
+                info!("Finished forward search");
+                (data_fwd, settled_fwd, nodes_settled)
+            });
+            let handle_bwd = s.spawn(|| {
+                info!("Start backward search");
+                let mut nodes_settled = 0;
+                let mut queue_bwd = BinaryHeap::new();
+                queue_bwd.push(Candidate::new(target, 0.0));
+
+                let mut data_bwd = FxHashMap::default();
+                let mut settled_bwd = FxHashSet::default();
+
+                data_bwd.insert(target, (0.0, None));
+
+                'outer: while !queue_bwd.is_empty() {
+                    if let Some(cand) = queue_bwd.pop() {
+                        // Stall on demand optimization
+                        for (_, edge) in self.g.edges_fwd(cand.node_idx) {
+                            if let Some((dist, _)) = data_bwd.get(&edge.source) {
+                                if *dist + edge.weight < cand.weight {
+                                    continue 'outer;
+                                }
+                            }
+                        }
+
+                        for (edge_idx, edge) in self.g.edges_bwd(cand.node_idx) {
+                            let new_distance = cand.weight + edge.weight;
+                            if new_distance
+                                < data_bwd
+                                    .get(&edge.source)
+                                    .unwrap_or(&(std::f64::INFINITY, None))
+                                    .0
+                            {
+                                data_bwd.insert(edge.source, (new_distance, Some(edge_idx)));
+                                queue_bwd.push(Candidate::new(edge.source, new_distance));
+                            }
+                        }
+                        nodes_settled += 1;
+                        settled_bwd.insert(cand.node_idx);
+                    }
+                }
+
+                info!("Finished backward search");
+                (data_bwd, settled_bwd, nodes_settled)
+            });
+            let (data_fwd, settled_fwd, nodes_settled_fwd) = handle_fwd.join().unwrap();
+            let (data_bwd, settled_bwd, nodes_settled_bwd) = handle_bwd.join().unwrap();
+
+            self.data_bwd = data_bwd;
+            self.data_fwd = data_fwd;
+            self.settled_fwd = settled_fwd;
+            self.settled_bwd = settled_bwd;
+            self.stats.nodes_settled = nodes_settled_fwd + nodes_settled_bwd;
+            self.stats.finish();
+        });
+
+        // Find the set `I` of nodes settled in both dijkstras
+        // let reader = self_arc.read().unwrap();
+        let intersect = self.settled_fwd.intersection(&self.settled_bwd);
+        let mut intersect_node = None;
+        // Find
+        // dist(s,t) = min { dist(s,v) + dist(v,t) | v in I}
+        // and remember intersect node `v`
+        let mut min_dist = std::f64::INFINITY;
+        for node in intersect {
+            let dist_fwd = self.data_fwd.get(node).unwrap().0;
+            let dist_bwd = self.data_bwd.get(node).unwrap().0;
+
+            let dist = dist_fwd + dist_bwd;
+            if dist < min_dist {
+                min_dist = dist;
+                intersect_node = Some(*node);
+            }
+        }
+
+        debug!("Intersection node: {:?}", intersect_node);
+        debug!("min {{ dist(s,v) + dist(t,v) | v in I }} = {}", min_dist);
+
+        self.reconstruct_shortest_path(intersect_node, source)
+    }
+
+    fn bwd_search(&mut self, target: NodeIndex) {
+        let mut queue_bwd = BinaryHeap::new();
+        queue_bwd.push(Candidate::new(target, 0.0));
+        self.data_bwd.insert(target, (0.0, None));
+
         'outer: while !queue_bwd.is_empty() {
             if let Some(cand) = queue_bwd.pop() {
                 // Stall on demand optimization
@@ -129,33 +251,44 @@ impl<'a> BiDirSearch<'a> {
                 self.settled_bwd.insert(cand.node_idx);
             }
         }
-
-        // Find the set `I` of nodes settled in both dijkstras
-        let intersect = self.settled_fwd.intersection(&self.settled_bwd);
-
-        // Find
-        // dist(s,t) = min { dist(s,v) + dist(v,t) | v in I}
-        // and remember intersect node `v`
-        let mut min_dist = std::f64::INFINITY;
-        for node in intersect {
-            let dist_fwd = self.data_fwd.get(node).unwrap().0;
-            let dist_bwd = self.data_bwd.get(node).unwrap().0;
-
-            let dist = dist_fwd + dist_bwd;
-            if dist < min_dist {
-                min_dist = dist;
-                intersect_node = Some(*node);
-            }
-        }
-
-        debug!("Intersection node: {:?}", intersect_node);
-        debug!("min {{ dist(s,v) + dist(t,v) | v in I }} = {}", min_dist);
-
-        self.stats.finish();
-
-        self.reconstruct_shortest_path(intersect_node, source)
     }
 
+    fn fwd_search(&mut self, source: NodeIndex) {
+        let mut queue_fwd = BinaryHeap::new();
+        queue_fwd.push(Candidate::new(source, 0.0));
+
+        self.data_fwd.insert(source, (0.0, None));
+
+        'outer: while !queue_fwd.is_empty() {
+            if let Some(cand) = queue_fwd.pop() {
+                // Stall on demand optimization
+                for (_, edge) in self.g.edges_bwd(cand.node_idx) {
+                    if let Some((dist, _)) = self.data_fwd.get(&edge.source) {
+                        if *dist + edge.weight < cand.weight {
+                            continue 'outer;
+                        }
+                    }
+                }
+
+                for (edge_idx, edge) in self.g.edges_fwd(cand.node_idx) {
+                    let new_distance = cand.weight + edge.weight;
+                    if new_distance
+                        < self
+                            .data_fwd
+                            .get(&edge.target)
+                            .unwrap_or(&(std::f64::INFINITY, None))
+                            .0
+                    {
+                        self.data_fwd
+                            .insert(edge.target, (new_distance, Some(edge_idx)));
+                        queue_fwd.push(Candidate::new(edge.target, new_distance));
+                    }
+                }
+                self.stats.nodes_settled += 1;
+                self.settled_fwd.insert(cand.node_idx);
+            }
+        }
+    }
     fn reconstruct_shortest_path(
         &mut self,
         intersect_node: Option<NodeIndex>,
@@ -222,9 +355,8 @@ impl<'a> BiDirSearch<'a> {
 
 #[cfg(test)]
 mod tests {
-    use core::num;
 
-    use approx::{abs_diff_eq, assert_abs_diff_eq, assert_ulps_eq};
+    use approx::assert_abs_diff_eq;
 
     use crate::{
         graph::node_index,
@@ -347,6 +479,40 @@ mod tests {
             assert_eq!(sp_ba, sp_bidir_ba);
         }
     }
+    fn test_search_par(overlay_graph: &OverlayGraph, a: usize, b: usize) {
+        let a = node_index(a);
+        let b = node_index(b);
+
+        let mut dijkstra = super::super::dijkstra::Dijkstra::new(overlay_graph.road_graph());
+        let sp_ab = dijkstra.search(a, b);
+        let sp_ba = dijkstra.search(b, a);
+
+        let mut bidir = BiDirSearch::new(overlay_graph);
+        let sp_bidir_ab = bidir.search_par(a, b);
+        let sp_bidir_ba = bidir.search_par(b, a);
+
+        if sp_ab.is_some() {
+            assert_abs_diff_eq!(
+                sp_ab.unwrap().weight,
+                sp_bidir_ab.unwrap().weight,
+                epsilon = 1e-4,
+            );
+        } else {
+            // Both should be None
+            assert_eq!(sp_ab, sp_bidir_ab);
+        }
+
+        if sp_ba.is_some() {
+            assert_abs_diff_eq!(
+                sp_ba.unwrap().weight,
+                sp_bidir_ba.unwrap().weight,
+                epsilon = 1e-4,
+            );
+        } else {
+            // Both should be None
+            assert_eq!(sp_ba, sp_bidir_ba);
+        }
+    }
 
     #[test]
     fn search_on_complex_graph() {
@@ -385,6 +551,26 @@ mod tests {
         runner
             .run(&(0..num_nodes, 0..num_nodes), |(a, b)| {
                 test_search(&overlay_graph, a, b);
+                Ok(())
+            })
+            .unwrap();
+    }
+    #[test]
+    fn search_par_on_vaterstetten() {
+        init_log();
+        let mut g = graph_vaterstetten();
+
+        let num_nodes = g.nodes.len();
+
+        let mut contractor = NodeContractor::new(&mut g);
+
+        let overlay_graph = contractor.run();
+
+        let mut runner = proptest::test_runner::TestRunner::default();
+
+        runner
+            .run(&(0..num_nodes, 0..num_nodes), |(a, b)| {
+                test_search_par(&overlay_graph, a, b);
                 Ok(())
             })
             .unwrap();
